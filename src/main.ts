1132907358907a113fd59dac5dad0b34630c2904
champ.wk <wirawat.champ@gmail.com>
2025-08-14 17:06:04 +0700

    refactor: 💡 update main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger("Main");
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, () => {
+    logger.log(`Server is running on port ${port}`);
+  });
 }
 bootstrap();
